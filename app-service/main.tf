6680fa9a4d0be5f3d0339fda4f8b7eb004c24843
d-yandrapu <55582117+d-yandrapu@users.noreply.github.com>
2020-01-29 15:32:25 +0530

    Update main.tf

--- app-service/main.tf
+++ app-service/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_app_service_plan" "test" {
-  name                = "terraform-appserviceplan"
+  name                = "terraform-appserviceplan-updated"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "test" {
 }
 
 resource "azurerm_app_service" "test" {
-  name                = "terraform-app-service"
+  name                = "terraform-app-service-updated"
   location            = "${azurerm_resource_group.test.location}"
   resource_group_name = "${azurerm_resource_group.test.name}"
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
