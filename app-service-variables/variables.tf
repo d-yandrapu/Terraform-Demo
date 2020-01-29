fc6ef57a4da3743ae00aecda2a267ac198db2e82
d-yandrapu <55582117+d-yandrapu@users.noreply.github.com>
2020-01-29 15:31:07 +0530

    Update variables.tf

--- app-service-variables/variables.tf
+++ app-service-variables/variables.tf
@@ -1,5 +1,5 @@
 variable "resource-group-name" {
-  default = "terraform-resource-group"
+  default = "terraform-resource-group-updated"
   description = "The prefix used for all resources in this example"
 }
 
@@ -9,6 +9,6 @@ variable "location" {
 }
 
 variable "app-service-name" {
-  default = "terraform-app-service"
+  default = "terraform-app-service-updated"
   description = "The name of the app service"
 }
